02fef14f6293882bac2c47205761cda4516258fa
Oskar <oskaras34@gmail.com>
2019-11-04 10:17:35 +0200

    working on Schema

---
+++ market_back/db/user.model.js
@@ -0,0 +1,29 @@
+// Creating Schema
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'This field is required'
+    },
+    age: {
+        type: Number,
+        required: 'This field is required'
+    },
+    email: {
+        type: String,
+        required: 'This field is required'
+    },
+    password: {
+        type: String,
+        required: 'This field is required'
+    },
+    team: {
+        type: Array
+    },
+    status: {
+        type: String,
+    }
+});
+
+mongoose.model('user', userSchema);
