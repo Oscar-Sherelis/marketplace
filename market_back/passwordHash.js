42d2c04386ee04ea5e801e1998f3e483583cea1c
Oskar <oskaras34@gmail.com>
2019-11-04 09:42:52 +0200

    Programming marketplace start

---
+++ market_back/passwordHash.js
@@ -0,0 +1,35 @@
+/**
+ * generates random string of characters i.e salt
+ * @function
+ * @param {number} length - Length of the random string.
+ */
+let genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2))
+            .toString('hex') /** convert to hexadecimal format */
+            .slice(0,length);   /** return required number of characters */
+};
+
+/**
+ * hash password with sha512.
+ * @function
+ * @param {string} password - List of required fields.
+ * @param {string} salt - Data to be validated.
+ */
+let sha512 = function(password, salt){
+    let hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+    hash.update(password);
+    let value = hash.digest('hex');
+    return {
+        salt:salt,
+        passwordHash:value
+    };
+};
+
+function saltHashPassword(userpassword) {
+    let salt = genRandomString(16); /** Gives us salt of length 16 */
+    let passwordData = sha512(userpassword, salt);
+    // console.log('UserPassword = '+userpassword);
+    // console.log('Passwordhash = '+passwordData.passwordHash);
+    // console.log('nSalt = '+passwordData.salt);
+    return passwordData.passwordHash;
+}
